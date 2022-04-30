521f0e5b35ad51c6c38b0fee3f7d3c9c118678c8
mpatel <mpatel@192.168.2.18>
2022-04-30 13:08:10 -0400

    Revert "Added new line"
    This reverts commit 5ab5d4bb7e5be78acb9f518b322d94b837178d87.

--- Smaple/src/smaple/sample.java
+++ Smaple/src/smaple/sample.java
@@ -4,7 +4,6 @@ public class sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Mayur Samiksha");
-		System.out.println("Mayur Samiksha");
 	}
 
 }
